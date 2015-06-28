dfee8c913b1bcfcd87eb8f37ae0efaacef7d8dab
ZeroPumpkin <zeropumpkin@gmail.com>
2015-06-28 15:22:35 +0100

    Merging controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -119,5 +119,67 @@ angular.module('starter.controllers', [])
     }.bind(this))  
   } 
 
-});
+})
 
+.controller('DecisionsCtrl', function($scope, Decisions) {
+  //init
+  $scope.decisions = {};
+  $scope.data      = {};
+
+  var DEBUG = Decisions.getDebug();
+
+  //retrieves all decisions
+  Decisions.getDecisions(function(result){
+    if(DEBUG){console.log('controller: getDecisions(', result, ')');}
+    $scope.$apply($scope.decisions = result);
+  });
+
+  //listens to DB updates - merges remote diffs (to be changed)
+  Decisions.synchDB(function(result){
+    if(DEBUG){console.log('controller: synchDB(', result, ')');}
+    $scope.$apply(function(){
+      _.each(result, function(element, index, list){
+        if(element._deleted){
+          delete($scope.decisions[element._id]);
+        } else {
+          $scope.decisions[element._id] = element;
+        }
+      })}
+    );
+  });
+
+  //adds a decision by passing a question
+  $scope.addDecision = function(){
+    if(DEBUG){console.log('controller: addDecision(', $scope.data.question, ')');}
+    Decisions.addDecision($scope.data.question); 
+    Decisions.getDecisions(function(result){
+      $scope.$apply($scope.decisions = result);
+    });
+    $scope.data.question = '';
+  }
+
+  //updates a decision
+  $scope.updateDecision = function(decision){
+    if(DEBUG){console.log('controller: updateDecision(', decision, ')');}
+    Decisions.updateDecision(decision);
+  }
+
+  //removes a decision
+  $scope.removeDecision = function(decision){
+    if(DEBUG){console.log('controller: removeDecision(', decision, ')');}
+    Decisions.removeDecision(decision);
+    Decisions.getDecisions(function(result){
+      $scope.$apply($scope.decisions = result);
+    });
+  }
+  
+  //adds an answer to a decision
+  $scope.setDecision = function(decision, answer){
+    if(DEBUG){console.log('controller: acceptDecision(', decision, ', ', answer, ')');}
+    Decisions.addAnswer(decision, answer);
+    Decisions.getDecisions(function(result){
+      $scope.$apply($scope.decisions = result);
+    });
+  }
+
+});
